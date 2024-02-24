2a4f79acf9a909f6e273376215235c2a53742481
Mona Lisa <j.emanuel.rdz.m@hotmail.com>
2024-02-24 00:15:10 -0600

    firebase core CI and auth with Andoid App

---
+++ lib/app/my_app.dart
@@ -0,0 +1,27 @@
+
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter App Demo",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+          title: const Text("App Coffee"),
+        ),
+        body: const Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
